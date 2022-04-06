27cfc6472ce7d14f1c0f20bdf5c4d87227bf4afb
Alex Salazar <alexwsalazar@gmail.com>
2022-04-06 02:02:28 -0500

    worked on shema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,33 @@
+-- Reset the DATABASE
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
+
+-- Create employee Table
 CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
 );
 
-CREATE TABLE role (
+-- Create roles table 
+CREATE TABLE roles (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    dapartment_id INT NOT NULL
+    salary DECIMAL NOT NULL,
+    dapartment_id INT NOT NULL,
+    FOREIGN KEY (dapartment)
+    REFERENCES dapartment(id)
+   
 );
+
+-- Create department table 
 
 CREATE TABLE dapartment (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    dept_name VARCHAR(30) NOT NULL
 );
